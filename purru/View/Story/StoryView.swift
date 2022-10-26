ce01ca791414760900ef7165f97ef3de1737234b
renakaagusta <renakaagusta28@gmail.com>
2022-10-26 17:37:16 +0700

    Add show hint

--- purru/View/Story/StoryView.swift
+++ purru/View/Story/StoryView.swift
@@ -300,6 +300,21 @@ struct StoryView: View {
         }
     }
     
+    func showHint() {
+        let camera = view.defaultCameraController
+            
+        let cameraDestination = view.scene?.rootNode.childNodes.filter({$0.name == "CAM BLUE"}).first
+            
+        camera.pointOfView?.position = SCNVector3(x: 0, y: 0, z: 0)
+            camera.pointOfView?.eulerAngles = SCNVector3(x: 0, y: 0, z: 0)
+            
+            hintVisibility = true
+            
+//            try await Task.sleep(nanoseconds: UInt64(seconds * Double(NSEC_PER_SEC)))
+            
+            hintVisibility = true
+        }
+    
     
     var body: some View {
         NavigationView {
@@ -345,8 +360,8 @@ struct StoryView: View {
                                 icon: Image(systemName: "lightbulb.fill"),
                                 color: Color.bg.primary,
                                 backgroundColor: Color.foot.primary,
-                                source: AppCircleButtonContentSource.Icon
-                                //onClick:
+                                source: AppCircleButtonContentSource.Icon,
+                                onClick: showHint
                             )
                             .padding()
                         }
@@ -357,8 +372,8 @@ struct StoryView: View {
                                 icon: Image(systemName: "lightbulb.fill"),
                                 color: Color.bg.primary,
                                 backgroundColor: Color.spot.primary,
-                                source: AppCircleButtonContentSource.Icon
-                                //onClick:
+                                source: AppCircleButtonContentSource.Icon,
+                                onClick: showHint
                             )
                             .padding()
                             .shadow(color: Color.spot.primary, radius: 15, x: 0, y: 0)
