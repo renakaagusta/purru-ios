93c8c5527320f3f6d810b2a1b5040c8e01924c20
renakaagusta <renakaagusta28@gmail.com>
2022-11-17 16:57:36 +0700

    Fix ripple effect

--- purru/View/Story/StoryView.swift
+++ purru/View/Story/StoryView.swift
@@ -40,7 +40,7 @@ struct StoryView: View {
     
     @State var pauseVisibility: Bool = false
     
-    @State private var focusedObjectIndex = 1
+    @State private var focusedObjectIndex = 4
     @State private var foundObject = 0
     
     @State private var elapsedTime: CGFloat = 0
@@ -155,7 +155,7 @@ struct StoryView: View {
                         SCNTransaction.begin()
                         SCNTransaction.animationDuration = 2
                     
-                        result.position.y = result.position.y + 20
+                        result.position.y = result.position.y + Float(data.height)
                         result.opacity = 0
                         
                         SCNTransaction.commit()
@@ -422,8 +422,6 @@ struct StoryView: View {
     }
     
     func playSoundEffect(soundName: String, soundExtention: String, currentTime: CGFloat?) {
-        print("===SOUND NAME===")
-        print(soundName)
         if(soundEffectPlayer != nil) {
             soundEffectPlayer?.stop()
         }
@@ -708,7 +706,7 @@ struct RippleView: View {
                         }
                         .position(x: x, y: y)
                 }.onAppear(perform: {
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.6, execute: {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.2, execute: {
                         self.isVisible = false
                         self.index = 0
                     })
